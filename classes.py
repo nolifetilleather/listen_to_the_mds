068de0ade63045ae6183ded65969840603dc6809
WhoopeeyGoldberg <rehtael.lit.efil.on@gmail.com>
2019-10-14 23:06:43 +0300

    class User in classes.py modified and renamed to UserState, main.py modified

--- classes.py
+++ classes.py
@@ -1,10 +1,6 @@
-class User:
+class UserState:
 
-    def __init__(
-        self,
-        telegram_id,
-    ):
-        self.telegram_id = telegram_id
+    def __init__(self):
 
         self.sort_type_selection_expected = False
 
@@ -17,5 +13,19 @@ class User:
 
         self.page = 0
         self.strng = ''
+        self.column = ''
 
         self.reversed_by_date_search_result = False
+
+    def reset(self):
+
+        self.sort_type_selection_expected = False
+        self.author_selection_expected = False
+        self.title_selection_expected = False
+        self.length_selection_expected = False
+        self.page_selection_expected = False
+        self.recording_selection_expected = False
+        self.page = 0
+        self.strng = ''
+        self.column = ''
+
--- main.py
+++ main.py
@@ -18,9 +18,17 @@ choice_keyboard.row('<<', '<', '>', '>>')
 @listen_to_the_mds_bot.message_handler(commands=['start'])
 def start(message):
 
-    if message.from_user.id not in users_states_dict:
+    user_id = message.from_user.id
+
+    # создание cоcтояния пользователя
+    if user_id not in users_states_dict:
+
+        user_state = UserState()
+        user_state.sort_type_selection_expected = True
+        users_states_dict[user_id] = user_state
+
         listen_to_the_mds_bot.send_message(
-            message.from_user.id,
+            user_id,
             (
                 'Бот предназначен для удобной навигации по '
                 'записям радиопередачи "Модель для сборки".\n'
@@ -28,8 +36,13 @@ def start(message):
             ),
         )
 
+    # сброс состояния пользователя
+    else:
+        users_states_dict[user_id].reset()
+
+    # это сообщение выводится в любом случае
     listen_to_the_mds_bot.send_message(
-        message.from_user.id,
+        user_id,
         (
             '/search_by_author\nПоиск по имени автора\n\n'
             '/search_by_title\nПоиск по названию\n\n'
@@ -38,61 +51,49 @@ def start(message):
         ),
     )
 
-    if message.from_user.id not in users_states_dict:
-        user = User(
-            telegram_id=message.from_user.id
-        )
-        user.sort_type_selection_expected = True
-        users_states_dict[message.from_user.id] = user
-
-    # сброс состояния пользователя
-    else:
-        del users_states_dict[message.from_user.id]
-        user = User(
-            telegram_id=message.from_user.id
-        )
-        users_states_dict[message.from_user.id] = user
-    print(users_states_dict)
-
-# состояние поиска по автору вкл
+# состояние поиска по автору "вкл"
 @listen_to_the_mds_bot.message_handler(commands=['search_by_author'])
 def search_by_author_set(message):
 
-    if message.from_user.id not in users_states_dict:
-        user = User(
-            telegram_id=message.from_user.id
-        )
-        user.author_selection_expected = True
-        users_states_dict[message.from_user.id] = user
+    user_id = message.from_user.id
 
-    elif message.from_user.id in users_states_dict:
-        del users_states_dict[message.from_user.id]
-        user = User(
-            telegram_id=message.from_user.id
-        )
-        user.author_selection_expected = True
-        user.page = 0
-        users_states_dict[message.from_user.id] = user
+    # создание cоcтояния пользователя
+    if user_id not in users_states_dict:
+        user_state = UserState()
+        user_state.author_selection_expected = True
+        users_states_dict[user_id] = user_state
 
+    # сброс и установка состояния пользователя
+    elif user_id in users_states_dict:
+        user = users_states_dict[user_id]
+        user.reset()
+        user.author_selection_expected = True
+
+    # это сообщение выводится в любом случае
     listen_to_the_mds_bot.send_message(
-        message.from_user.id,
-        (
-            'Введите имя автора'
-        ),
+        user_id,
+        'Введите имя автора',
     )
 
 @listen_to_the_mds_bot.message_handler(content_types=['text'])
 def search_by_strng(message):
+
+    user_id = message.from_user.id
+
     if (
-            message.from_user.id in users_states_dict
+            user_id in users_states_dict
             and
-            (users_states_dict[message.from_user.id].author_selection_expected
+            (users_states_dict[user_id].author_selection_expected
              or
-             users_states_dict[message.from_user.id].title_selection_expected)
+             users_states_dict[user_id].title_selection_expected)
     ):
+        
+        user_state = users_states_dict[user_id]
+
+        # "перестаем ожидать" ввод подстроки для поиска
+        user_state.reset()
         # сохраняем запрос пользователя
-        users_states_dict[message.from_user.id].strng = message.text
-        # "перестаем ожидать" вво
+        user_state.strng = message.text
 
         pages_dict = dict_with_pages_for_navigation(
             sorted_by_strng_recordings_list(
